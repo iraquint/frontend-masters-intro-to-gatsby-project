258ab9943955f0c5d8255b748986a408ccca882a
Ira Quint <IQuint3820@gmail.com>
2022-03-11 14:54:56 -0500

    add and use a shared layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default function AboutPage() {
   return (
-    <main>
+    <Layout
+      title="About This Site"
+      description="More information about this site"
+    >
       <h1>About this site</h1>
       <Link to="/">Home</Link>
-    </main>
+    </Layout>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,13 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import Layout from '../components/layout';
 
 // Pages in Gatsby have to be a default export
 export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query GetSiteTitle {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  const meta = data?.site?.siteMetadata ?? {};
-
   return (
-    <>
-      <header>
-        <Link to="/">{meta.title}</Link>
-      </header>
-      <main>
-        <h1>Hello Frontend Masters</h1>
-        <Link to="/about">About</Link>
-      </main>
-    </>
+    <Layout>
+      <h1>Hello Frontend Masters</h1>
+      <Link to="/about">About</Link>
+    </Layout>
   );
 }
