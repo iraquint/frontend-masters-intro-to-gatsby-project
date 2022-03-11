ee1118e1d922cdf5ecd7d213c6a041a5875349b4
Ira Quint <IQuint3820@gmail.com>
2022-03-11 11:28:10 -0500

    Add pages

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+// Pages in Gatsby have to be a default export
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hello Frontend Masters</h1>
+    </main>
+  );
+}
