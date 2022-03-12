58efc8ca71740c36ceaea6d4e6bbda8c34c9692c
Ira Quint <IQuint3820@gmail.com>
2022-03-12 09:26:55 -0500

    Add posts to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,38 @@ import Layout from '../components/layout';
 
 // Pages in Gatsby have to be a default export
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            title
+            date(fromNow: true)
+            description
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = data.allMdx.nodes;
+
   return (
     <Layout>
       <h1>Hello Frontend Masters</h1>
       <Link to="/about">About</Link>
+
+      <h2>Check out my recent blog posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+            <small>posted {post.frontmatter.date}</small>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
