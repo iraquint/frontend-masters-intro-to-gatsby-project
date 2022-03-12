4de99aca0b043d4e7bfb3a3c2535914a25f196cc
Ira Quint <IQuint3820@gmail.com>
2022-03-12 12:19:53 -0500

    Image handling and optimization

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,38 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 
-export default function AboutPage() {
+/*
+Page query
+- injected into the component as data at build time
+
+This is an example for the sake of example, you'd want to use
+useStaticQuery here
+
+However, page queries have access to the page context
+*/
+
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
     <Layout
       title="About This Site"
       description="More information about this site"
     >
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="a cocktail set inside an elaborate floral arrangement with dry ice mist curling around it"
+      />
       <h1>About this site</h1>
       <Link to="/">Home</Link>
     </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
+
+import { imageWrapper } from '../styles/index.module.css';
 
 // Pages in Gatsby have to be a default export
 export default function IndexPage() {
@@ -24,6 +27,16 @@ export default function IndexPage() {
 
   return (
     <Layout>
+      <div className={imageWrapper}>
+        <StaticImage
+          src="../images/ivana-la-61jg6zviI7I-unsplash.jpg"
+          alt="a corgi sitting on a bed with red paper hearts all over it. it looks unamused"
+          placeholder="dominantColor"
+          width={300}
+          height={300}
+        />
+      </div>
+
       <h1>Hello Frontend Masters</h1>
       <Link to="/about">About</Link>
 
